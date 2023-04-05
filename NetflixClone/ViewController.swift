566100dadea432c50b74fe5841652fac16bcf57f
Azamat Yerbossyn <60841145+azayerbosyn@users.noreply.github.com>
2023-04-05 12:28:32 +0600

    Initial Commit

---
+++ NetflixClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixClone
+//
+//  Created by Azamat Yerbossyn on 05.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
